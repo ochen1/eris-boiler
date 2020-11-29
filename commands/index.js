da60a212d264489b9ffd1e0fae395b4e7d680b97
Tet <ic3dplasma@gmail.com>
2020-11-28 22:55:48 -0800

    Did some changes

---
+++ commands/index.js
@@ -0,0 +1,8 @@
+module.exports = {
+  isIndex: true,
+  settings: require('./settings'),
+  status: require('./status'),
+  help: require('./help'),
+  permissions: require('./permissions'),
+  process: require('./process')
+}
